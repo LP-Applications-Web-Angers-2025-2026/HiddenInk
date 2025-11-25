4b8f54682b38e70c455147726016d8d4b30d6892
Gamblin Vincent <yuumie31@gmail.com>
2025-11-25 14:56:41 +0100

    update GUI

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -1,8 +1,21 @@
 #include <QApplication>
 #include "mainwindow.h"
+#include <locale>
+#include <clocale>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 int main(int argc, char *argv[])
 {
+#ifdef _WIN32
+    // Ensure the Windows console (if shown) uses UTF-8 so accented characters print correctly
+    SetConsoleOutputCP(CP_UTF8);
+    // Also set C locale to the user's default to help Qt/local functions
+    std::setlocale(LC_ALL, "");
+#endif
+
     QApplication app(argc, argv);
     MainWindow w;
     w.show();
